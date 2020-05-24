ae65d99d33cd011fffe1ff092c0ad7529b072d47
Felipe Mesquita Uchoa <femeuf5@gmail.com>
2020-05-24 17:35:04 -0300

    Updated Manifest

---
+++ app/src/main/java/com/femeuc/decidaahistria/StoriesActivity.java
@@ -0,0 +1,13 @@
+package com.femeuc.decidaahistria;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StoriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stories);
+    }
+}
