5a4dd8bb5501b6e632c215ed0345a777f598063b
Felipe Mesquita Uchoa <femeuf5@gmail.com>
2020-05-24 17:38:02 -0300

    Updated MySingleton class

--- app/src/main/java/com/femeuc/decidaahistria/MySingleton.java
+++ app/src/main/java/com/femeuc/decidaahistria/MySingleton.java
@@ -1,8 +1,6 @@
 package com.femeuc.decidaahistria;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.util.LruCache;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -41,3 +39,45 @@ public class MySingleton {
     }
 
 }
+
+
+
+
+
+
+////        final TextView testTextview = findViewById(R.id.testTextView);
+//
+//    RequestQueue requestQueue;
+//
+//    // Instantiate the cache
+//    Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+//
+//    // Set up the network to use HttpURLConnection as the HTTP client.
+//    Network network = new BasicNetwork(new HurlStack());
+//
+//// Instantiate the RequestQueue with the cache and network.
+//                requestQueue = new RequestQueue(cache, network);
+//
+//                        // Start the queue
+//                        requestQueue.start();
+//
+//                        String url = "https://decida-a-historia.herokuapp.com/users/1";
+//
+//                        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+//                        (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+//
+//@Override
+//public void onResponse(JSONObject response) {
+////                        testTextview.setText(response.toString());
+//        }
+//        }, new Response.ErrorListener() {
+//
+//@Override
+//public void onErrorResponse(VolleyError error) {
+//        // TODO: Handle error
+////                        testTextview.setText("Error: " + error.toString());
+//        }
+//        });
+//
+//        // Access the RequestQueue through your singleton class.
+//        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
