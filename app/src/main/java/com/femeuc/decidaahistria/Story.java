c7e977015f639bc71d9afa93d8a764b4708a6aec
Felipe Mesquita Uchoa <femeuf5@gmail.com>
2020-05-24 17:42:51 -0300

    Created a class to represent each Story

---
+++ app/src/main/java/com/femeuc/decidaahistria/Story.java
@@ -0,0 +1,49 @@
+package com.femeuc.decidaahistria;
+
+public class Story {
+    private int id;
+    private String genre;
+    private String title;
+    private String description;
+    private int beginning_page_id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getBeginning_page_id() {
+        return beginning_page_id;
+    }
+
+    public void setBeginning_page_id(int beginning_page_id) {
+        this.beginning_page_id = beginning_page_id;
+    }
+}
